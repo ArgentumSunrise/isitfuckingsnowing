eace5a0605e458a2468e4bf88b5beb023386234f
ArgentumSunset <coltergiem@gmail.com>
2016-03-06 23:18:52 -0700

    Screwed around, mainly

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function (position) {
                 console.log('hello2');
-                isItSnowing(position.coords.latitude + ',' + position.coords.longitude);
+                isItSnowing(position.coords.latitude, position.coords.longitude);
             });
         } else {
             showMyError("Your browser doesn't support geolocation.");
@@ -20,21 +20,6 @@
         });
     }
 
-    function isItSnowing(location, woeid) {
-        $.simpleWeather({
-            location: location,
-            woeid: woeid,
-            unit: 'f',
-            success: function (weather) {
-                console.log("hello3");
-                if ($.inArray(weather.code, codeArray)) {
-                    $('#inner h1').text("IT'S FUCKING SNOWING.");
-                    $('#inner:after').css("content", "url(imgs/snow.png)");
-                }
-            },
-            error: function (error) {
-                console.log(error);
-                showMyError(error);
-            }
-        })
+    function isItSnowing(long, lat) {
+
     }
