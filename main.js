2f510d6e08ce889083aa1f9d9686afee2c27fa22
ArgentumSunset <coltergiem@gmail.com>
2016-03-07 22:30:12 -0700

    Made it go with Yahoo whatever

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
     var codeArray = [13, 14, 15, 16, 41, 42, 43, 46];
+    var APPID = ''; // Your Yahoo Application ID
+    var DEG = 'c'; // c for celsius, f for fahrenheit
 
     $(document).ready(function () {
         console.log('hello');
@@ -20,6 +22,47 @@
         });
     }
 
-    function isItSnowing(long, lat) {
+    function isItSnowing(lon, lat) {
+
+        // Yahoo's PlaceFinder API http://developer.yahoo.com/geo/placefinder/
+        // We are passing the R gflag for reverse geocoding (coordinates to place name)
+        var geoAPI = 'http://where.yahooapis.com/geocode?location=' + lat + ',' + lon + '&flags=J&gflags=R&appid=' + APPID;
+
+        // Forming the query for Yahoo's weather forecasting API with YQL
+        // http://developer.yahoo.com/weather/
+
+        var wsql = 'select * from weather.forecast where woeid=WID and u="' + DEG + '"',
+            weatherYQL = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent(wsql) + '&format=json&callback=?',
+            code, city, results, woeid;
+
+        // Issue a cross-domain AJAX request (CORS) to the GEO service.
+        // Not supported in Opera and IE.
+        $.getJSON(geoAPI, function (r) {
+
+            if (r.ResultSet.Found == 1) {
+
+                results = r.ResultSet.Results;
+                code = results[0].city;
+                code = results[0].item.code;
+                alert(code);
+
+                //                // This is the city identifier for the weather API
+                //                woeid = results[0].woeid;
+                //
+                //                // Make a weather API request (it is JSONP, so CORS is not an issue):
+                //                $.getJSON(weatherYQL.replace('WID', woeid), function (r) {
+                //
+                //                    if (r.query.count == 1) {
+                //
+                //                    } else {
+                //                        showMyError("Error retrieving weather data!");
+                //                    }
+                //                });
+
+            }
+
+        }).error(function () {
+            showError("Your browser does not support CORS requests!");
+        });
 
     }
